dda954a6ef3b7a316525f729796c70e0ad2975fa
ZMHastings1 <157257781+ZMHastings1@users.noreply.github.com>
2024-01-28 18:41:50 -0600

    Add files via upload

---
+++ Homework2-Q1.cs
@@ -0,0 +1,44 @@
+﻿namespace Homework2;
+//Program to input grade letters
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Console line to prompt the user to enter a grade
+       Console.Write("Enter a letter grade (A, B, C, D, F): ");
+        char grade = char.ToUpper(Console.ReadKey().KeyChar);
+        Console.WriteLine(); 
+
+        int gpaPoints;
+
+        //If statements for the different letter grades
+        if (grade == 'A')
+        {
+            gpaPoints = 4;
+        }
+        else if (grade == 'B')
+        {
+            gpaPoints = 3;
+        }
+        else if (grade == 'C')
+        {
+            gpaPoints = 2;
+        }
+        else if (grade == 'D')
+        {
+            gpaPoints = 1;
+        }
+        else if (grade == 'F')
+        {
+            gpaPoints = 0;
+        }
+        else
+        {
+            Console.WriteLine("Wrong Letter Grade!");
+            return; // Exit the program if the letter grade is incorrect
+        }
+
+        Console.WriteLine($"GPA Points for grade {grade}: {gpaPoints}");
+    }
+}
+
