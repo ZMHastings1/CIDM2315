b4e7e16cc353e5a2a8f2493073fb11f94c036944
ZMHastings1 <157257781+ZMHastings1@users.noreply.github.com>
2024-01-28 19:06:12 -0600

    Add files via upload

---
+++ Homework2-Q2.cs
@@ -0,0 +1,44 @@
+﻿namespace Homework2_Q2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter three numbers:");
+
+        // Input the first number
+        Console.Write("Please input the first number: ");
+        double number1 = Convert.ToInt16(Console.ReadLine());
+
+        // Input the second number
+        Console.Write("Please input the second number: ");
+        double number2 = Convert.ToInt16(Console.ReadLine());
+
+        // Input the third number
+        Console.Write("Please input the third number: ");
+        double number3 = Convert.ToInt16(Console.ReadLine());
+
+        // Find the smallest number
+        double smallest = FindSmallest(number1, number2, number3);
+
+        // Print the result
+        Console.WriteLine($"The smallest number is: {smallest}");
+    }
+
+    static double FindSmallest(double number1, double number2, double number3)
+    {
+        // Compare the three numbers using if statements and return the smallest
+        if (number1 <= number2 && number1 <= number3)
+        {
+            return number1;
+        }
+        else if (number2 <= number1 && number2 <= number3)
+        {
+            return number2;
+        }
+        else
+        {
+            return number3;
+        }
+    }
+}
